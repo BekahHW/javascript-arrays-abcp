13b541fee8242a3a69de6015c25c6a47ccacce0e
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-08 19:17:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,3 +37,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+
+function removeElementFromEndOfArray(array){
+  newArr = array.slice(0, array.length -1)
+  return nnewArr
+}
