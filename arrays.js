0dce0288685963dddf07e32ba5622d072541c054
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-08 18:56:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element){
- newArr= array.unshift
+ newArr= array.unshift(element)
  return newArr
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift
+  array.unshift(element)
   return array
 }
