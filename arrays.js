0a2e78c610dcf0d552ae5a889f3f93a26667b790
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-08 19:07:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  newArr= [array,... (element)]
+  newArr= [...array,element]
   return newArr
 }
 
